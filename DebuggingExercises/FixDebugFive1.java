e23493566b8e8369f4a158c267c2a57d24c2846d
jakobotten <jakob.otten@my.jcu.edu.au>
2017-08-23 00:06:26 +1000

    Fix bugs

--- DebuggingExercises/FixDebugFive1.java
+++ DebuggingExercises/FixDebugFive1.java
@@ -4,7 +4,7 @@
 // Grilled cheese and fish are $1.99
 // Fries are 89 cents
 import javax.swing.*;
-public class DebugFive1
+public class FixDebugFive1
 {
    public static void main(String args[]) throws Exception
    {
@@ -17,15 +17,15 @@ public class DebugFive1
       usersChoiceString = JOptionPane.showInputDialog(null,
          "Order please\n1 - Burger\n2 - Hotdog" +
          "\n3 - Grilled cheese\n4 - Fish sandwich");
-      usersChoice= Integer.parseInt(usersChoice);
-      if(usersChoice == 1 && usersChoice == 2)
-         bill = bill + LOW_PRICE;
+      usersChoice= Integer.parseInt(usersChoiceString);
+      if(usersChoice == 1 || usersChoice == 2)
+         bill = bill + HIGH_PRICE;
       else
-         bill = bill - MED_PRICE;
+         bill = bill + MED_PRICE;
       usersChoiceString = JOptionPane.showInputDialog(null,
           "Fries with that?\n1 - Yes\n2 - No");
-      usersChoice = Integer.parse(usersChoiceString);
-      if (usersChoice == 1);
+      usersChoice = Integer.parseInt(usersChoiceString);
+      if (usersChoice == 1)
           bill = bill + LOW_PRICE;
       JOptionPane.showMessageDialog(null,"Bill is " + bill);
    }
